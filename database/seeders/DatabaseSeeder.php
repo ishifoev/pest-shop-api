7b15b53a94003856cdb6345f282a772913e403ca
ishifoev <alex.malikov94@gmail.com>
2023-09-07 09:42:13 +0500

    Add seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Ramsey\Uuid\Uuid;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +15,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // Check if the admin user already exists
+        $adminExists = User::where('email', 'admin@buckhill.co.uk')->exists();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        if (!$adminExists) {
+            // If the admin user doesn't exist, create it
+            $adminUuid = Uuid::uuid4()->toString();
+
+            User::create([
+                'uuid' => $adminUuid,
+                "name" => "Admin",
+                'email' => 'admin@buckhill.co.uk',
+                'password' => Hash::make('admin'),
+                'first_name' => 'Admin',
+                'last_name' => 'Buckhill',
+                'is_admin' => true,
+                'address' => 'Aini 26 app 7',
+                'phone_number' => '908997474',
+            ]);
+        }
     }
 }
